1c22ab23ed38c70d5251337fe4b8e6fdeaed9fbe
lexxx3105 <129284703+lexxx3105@users.noreply.github.com>
2024-01-18 20:23:12 +0300

    add file

--- src/app.js
+++
@@ -1,5 +0,0 @@
-import Game from './game';
-
-const myGame = new Game();
-myGame.start();
-import { Game, GameSavingData, readGameSaving, writeGameSaving as saveGame, readGameSaving as loadGame } from './game';
